c9e427233e9ba6d23c92bd6ce630dd6e6a175e45
mariasauras <saurasm02@gmail.com>
2023-01-03 00:32:55 +0100

    update Text_preprocessing class

--- src/basic_project.py
+++ src/basic_project.py
@@ -5,14 +5,17 @@
 #==============================================================#
 
 ######## Required libraries ########
-import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from nltk.tokenize import wordpunct_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
+import nltk
+from gensim.utils import tokenize
+import re  # For preprocessing
+import pandas as pd  # For data handling
+from collections import defaultdict  # For word frequency
+import spacy  # For preprocessing
+import logging  # Setting up the loggings to monitor gensim
+logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
 
 
 def load_df(df):
@@ -34,34 +37,21 @@ class Text_preprocessing():
     
     # Processing dataframe.
     def reviews(self,column):
-        return self.df[column]
+        df = self.df[column]
+        return df.dropna().reset_index(drop=True)
     
-    ## STEP 1. TOKENIZATION
-    def tokenization(self):
-        rvw = self.reviews('Review Text')
-        return [wordpunct_tokenize(t) for t in sent_tokenize(rvw)]
-    
-    ## STEP 2. HOMOGENIZATION
-    def homogenization(self):
-        rvw = self.tokenization()
-        '''
-        review_tokens_filtered = []
-        for token in rvw:
-            if token.isalnum(): review_tokens_filtered.append(token.lower())
-        return review_tokens_filtered
-        '''
-        # Lower-cased of the tokens and elimination of non-alphanumeric characters
-        review_tokens_filtered =  [token.lower() for token in rvw if token.isalnum() ]
-        # Word normalization --> Lemmatization
-        wnl = WordNetLemmatizer()
-        return [wnl.lemmatize(el) for el in review_tokens_filtered]
+    def homogenization(self, doc):
+        # Lemmatizes and removes stopwords
+        txt = [token.lemma_ for token in doc if not token.is_stop]
+        # Word2Vec uses contextual words to learn the vector representation of a target word, 
+        # if a sentence has only one or two words, the training benefit is very small so we eliminate it.
+        if len(txt) > 2: return ' '.join(txt)
 
-    ## STEP 3. Removing those words that are very common in language and 
-    # do not carry out useful semantic content --> Removing stopwords
-    def cleaning_data(self):
-        sw = stopwords.words('english')
-        l = self.homogenization()
-        return [word for word in l if word not in sw]
+    # We are lemmatizing and removing the stopwords and non-alphabetic characters for each line of dialogue.
+    def cleaning_data(self,data):
+        nlp = spacy.load("en_core_web_sm") # para acelerar el proceso de limpieza
+        cleaning = (re.sub("[^A-Za-z']+", ' ', str(row)).lower() for row in data)
+        return [self.homogenization(doc) for doc in nlp.pipe(cleaning, batch_size=5000)]
 
 
 #==============================================================#
